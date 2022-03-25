bad74aeb28d90c99cae9697903bdda2375ae1cb1
Axel <clair.axel21@hotmail.com>
2022-03-25 15:37:39 +0100

    method POST dans la fonction pour calculer le prix TTC

--- form.php
+++ form.php
@@ -7,23 +7,38 @@
     <title>Document</title>
 </head>
 <body>
-    <form action= "" method="GET">
-        <p>saisir un premier nombre : </p>
+    <form action= "" method="POST">
+        <p>Saisir le prix d'un article : </p>
         <input type="text" name="nbr1">
-        <p>saisir un second nombre : </p>
+        <p>Saisir le nombre d'article : </p>
         <input type="text" name="nbr2">
+        <p>Saisir le taux de TVA : </p>
+        <input type="text" name="nbr3">
         <br>
         <br>
         <input type="submit" value="Calculer">
     </form>
     <?php
-        if(isset($_GET["nbr1"]) AND isset($_GET["nbr2"]) AND $_GET["nbr1"] != "" AND $_GET["nbr2"] != ""){
-            $nombre1 = $_GET["nbr1"];
-            $nombre2 = $_GET["nbr2"];
-            $total = $nombre1 + $nombre2;
-            echo "le résultat de $nombre1 + $nombre2 est égale à $total";
+        // if(isset($_GET["nbr1"]) AND isset($_GET["nbr2"]) AND $_GET["nbr1"] != "" AND $_GET["nbr2"] != ""){
+        //     $nombre1 = $_GET["nbr1"];
+        //     $nombre2 = $_GET["nbr2"];
+        //     $total = $nombre1 + $nombre2;
+        //     echo "le résultat de $nombre1 + $nombre2 est égale à $total";
+        // }else{
+        //     echo "Veuillez Remplir les champs";
+        // }
+    ?>
+    <?php
+        if(isset($_POST["nbr1"]) AND isset($_POST["nbr2"]) AND isset($_POST["nbr3"])
+        AND $_POST["nbr1"] != "" AND $_POST["nbr2"] != "" AND $_POST["nbr3"] != ""){
+            function calcule($prixHt,$nbrArticle,$tva){
+                $prixTTC = $prixHt*$nbrArticle*(($tva/100)+1);
+                return $prixTTC;
+            }
+            echo " le prix de votre article HT en prix TTC est égale à ";
+            echo calcule($_POST["nbr1"],$_POST["nbr2"],$_POST["nbr3"]);
         }else{
-            echo "Veuillez Remplir les champs";
+                echo "Veuillez saisir les information";
         }
     ?>
 </body>
